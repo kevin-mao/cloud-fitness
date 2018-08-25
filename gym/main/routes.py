1cb992f38380ba2677dae34a9671758c63d75db5
Kevin Mao <km3290@columbia.edu>
2018-08-25 11:05:53 -0400

    'merging'

--- gym/main/routes.py
+++ gym/main/routes.py
@@ -55,7 +55,7 @@ def results(query):
                 place_id = result['place_id']
                 #it does have a google maps link tho (specific to each location)
                 maps_link = get_place_details(place_id)['result']['url']
-                name = result['name']
+                gym_name = result['name']
                 address = result['formatted_address']
                 lat = result['geometry']['location']['lat']
                 lng = result['geometry']['location']['lng']
@@ -64,7 +64,7 @@ def results(query):
                 #image = result['photo_reference']
 
                 #get link and description with web scraping 
-                link_and_description = scrape(query, name)
+                link_and_description = scrape(query, gym_name)
                 link = link_and_description[0]
                 description = link_and_description[1]  
 
