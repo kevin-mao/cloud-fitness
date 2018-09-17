460ee9e93c53d5ba8a00f982bed731a54badd7a9
Kevin <km3290@columbia.edu>
2018-09-17 16:32:30 -0400

    enter a location

--- gym/search/forms.py
+++ gym/search/forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
-    search=StringField('Search for free gym passes at gyms near you!', render_kw={"placeholder": "Enter a city"}, validators=[DataRequired()])
+    search=StringField('Search for free gym passes at gyms near you!', render_kw={"placeholder": "Enter a location"}, validators=[DataRequired()])
     range =IntegerField('Search distance in miles', render_kw={"placeholder": "Search distance in miles"})
     submit=SubmitField('Search')
